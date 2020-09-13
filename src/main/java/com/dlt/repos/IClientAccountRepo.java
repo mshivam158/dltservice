bed8caa2e880cf8dfa1a7173541b8bb1f3943250
unknown <shvmmishra.158@gmail.com>
2020-09-12 23:33:57 -0400

    annoted repositor

--- src/main/java/com/dlt/repos/IClientAccountRepo.java
+++ src/main/java/com/dlt/repos/IClientAccountRepo.java
@@ -1,9 +1,11 @@
 package com.dlt.repos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.dlt.model.ClientAccount;
 
+@Repository
 public interface IClientAccountRepo extends JpaRepository<ClientAccount, Long> {
 
 }
